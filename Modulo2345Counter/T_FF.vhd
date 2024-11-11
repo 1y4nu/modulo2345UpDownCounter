4400006c033c978713588891b93626e8ded42b09
Michael Iyanuoluwa Akano <s9ykk@unb.ca>
2024-11-11 17:16:59 -0400

    Developed Low Level Flip FLop

--- Modulo2345Counter/T_FF.vhd
+++ Modulo2345Counter/T_FF.vhd
@@ -1,29 +1,36 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
 entity T_FF is 
-	port (
-		CLK, T, CL, PR : in std_logic;
-		Q, Qn : out std_logic
-	);
+    port (
+        En, UD, CLK, T, CL, PR : in std_logic;
+        Q, Qn : out std_logic
+    );
 end T_FF;
+
 architecture logic_func of T_FF is 
-	signal Qt : std_logic;
-	begin
-		process(CLK, T, CL, PR)
-			begin
-				-- asynchronous clear and preset with XOR Condition
+    signal Qt : std_logic;
+begin
+	process(CLK)
+		begin
+			if rising_edge(CLK) then
+				-- Synchronous Clear and Preset
 				if (CL = '0') then
-					Qt <= '0';
-				elsif (PR <= '0') then
-					Qt <= '1';
-					
-				-- Synchronous Toggle on Rising Edge of Clock
-				elsif rising_edge(CLK) then
-					if (T = '1') then
-						Qt <= NOT(Qt);
-					end if;
-				end if;
-		end process;
-	Q <= Qt;
-	Qn <= NOT(Qt);
+                Qt <= '0';  -- Clear
+            elsif (PR = '0') then
+                Qt <= '1';  -- Preset
+            elsif (T = '1') then
+                Qt <= NOT(Qt);  -- Toggle
+            end if;
+			end if;
+		
+			if (UD = '0') then
+				Q <= Qt and En;
+			elsif (UD = '1') then
+				Q <= not(Qt) and En;
+			end if;
+    
+	 end process;
+	 
+	 Qn <= NOT(Qt);
 end logic_func;
