84122b2a9bfdcc19e905552dbcdc1f0a577febb5
Michael Iyanuoluwa Akano <s9ykk@unb.ca>
2024-11-21 18:47:53 -0400

    Added one more stage to implement 13 bits for 13 switches

--- UpDownCounter.vhd
+++ UpDownCounter.vhd
@@ -7,13 +7,13 @@ entity UpDownCounter is
         Cl : in STD_LOGIC;
         UD : in STD_LOGIC;   -- '1' for down, '0' for up
 		  Load : in STD_LOGIC;
-		  Load_Value : in STD_LOGIC_VECTOR(11 downto 0);
-        Q : out STD_LOGIC_VECTOR(11 downto 0)
+		  Load_Value : in STD_LOGIC_VECTOR(12 downto 0);
+        Q : out STD_LOGIC_VECTOR(12 downto 0)
     );
 end UpDownCounter;
 
 architecture Behavioral of UpDownCounter is
-    signal Qt : STD_LOGIC_VECTOR(11 downto 0);
+    signal Qt : STD_LOGIC_VECTOR(12 downto 0);
 	 signal clear : STD_LOGIC; -- Internal clear signal
 	 
 	 
@@ -159,6 +159,17 @@ begin
 		  Load => Load,
 		  Load_Value => Load_Value(11),
         Q => Qt(11)
+    );
+	 
+	 stage12 : T_FlipFlop port map (
+        T => (Qt(0) AND Qt(1) AND Qt(2) AND Qt(3) AND Qt(4) AND Qt(5) AND Qt(6) AND Qt(7) AND Qt(8) AND Qt(9) AND Qt(10) AND Qt(11) AND NOT(UD)) OR 
+		  (NOT(Qt(0)) AND NOT(Qt(1)) AND NOT(Qt(2)) AND NOT(Qt(3)) AND NOT(Qt(4)) AND NOT(Qt(5)) AND NOT(Qt(6)) AND NOT(Qt(7)) AND NOT(Qt(8)) AND NOT(Qt(9)) AND NOT(Qt(10)) AND NOT(Qt(11)) AND UD),
+		  
+        clk => Clk,
+        cl => clear,
+		  Load => Load,
+		  Load_Value => Load_Value(12),
+        Q => Qt(12)
     );
 	 
 
