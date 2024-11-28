cae300f0103bfbbc56035ae5f3f0e72959afb886
Michael Iyanuoluwa Akano <s9ykk@unb.ca>
2024-11-28 16:01:24 -0400

    Removed Unused Component

--- UpDownCounter.vhd
+++ UpDownCounter.vhd
@@ -33,12 +33,6 @@ architecture logic of UpDownCounter is
         );
     end component;
 	 
-	 component sevenSegment is
-	port(
-		A, B, C, D : in std_logic; --input
-		fa, fb, fc, fd, fe, ff, fg : out std_logic --output
-		);
-	end component;
 	 
 	component clk_gen_1_output is
 		  generic( n  : integer := 25000;
